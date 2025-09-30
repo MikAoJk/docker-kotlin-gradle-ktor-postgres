b2f25e74e913e3fb89a741f2ced98dc9bb8c020d
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-09-30 10:57:05 +0200

    Update JUnit Jupiter version to 6.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ group = "io.github.MikAoJk"
 version = "1.0.0"
 
 val ktorVersion = "3.3.0"
-val junitJupiterVersion = "5.13.4"
+val junitJupiterVersion = "6.0.0"
 val logbackVersion = "1.5.18"
 val logstashEncoderVersion = "8.1"
 val kotlinVersion = "2.2.20"
